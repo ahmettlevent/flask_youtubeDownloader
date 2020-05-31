099f99c552e52bed3aa2c0b28d918941277a05b1
ahmettlevent <ahmettlevent@users.noreply.github.com>
2020-05-31 13:50:51 +0300

    update v0.1

---
+++ webapps/youtube_api.py
@@ -0,0 +1,66 @@
+import youtube_dl
+import sys
+import random
+import os
+allformats = {'140': 'mp3 128k','141': 'mp3 256k','139': 'mp3 48', '17': 'gp3 144p', '36': 'gp3 240p', '5': 'flv', '43': 'webm', '18': 'mp4 360p', '22': 'mp4 720p'}
+
+def search_video(url, output='audio', quiet=False):
+    url = url.split("=")[-1]
+    avaible={}
+    avaible["bestaudio"]={"name":"Best AUDIO","url":"/download/{}/mp3-bestaudio".format(url)}
+    avaible["audio"]={"name":"Normal AUDIO","url":"/download/{}/mp3-audio".format(url)}
+    #avaible["bestvideo"]={"name":"Best VIDEO","url":"/download/{}/mp4-bestvideo+bestaudio".format(url)}
+    #avaible["video"]={"name":"Normal VIDEO","url":"/download/{}/mp4-best".format(url)}
+
+    return avaible
+
+
+def download_source(url,type,id):
+    url = "https://www.youtube.com/watch?v="+url
+    print(id)
+    if id == "bestaudio" or id == "audio":
+        ydl_opts = {
+            'outtmpl': 'database/music/%(title)s-%(upload_date)s-%(id)s.%(ext)s',
+            'format': id+"/best",
+            'ffmpeg_location':"paths/ffmpeg/",
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+                }]}
+    elif id == "bestvideo+bestaudio" or id == "best":
+        ydl_opts = {
+            'outtmpl': 'database/video/%(title)s-%(upload_date)s-%(id)s.%(ext)s',
+            'format': id+"[ext=mp4]"+"/best"+"[ext=mp4]",
+            'ffmpeg_location':"paths/ffmpeg/",
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                "preferedformat":"mp4"
+                }]}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+        result = ydl.extract_info(url, download=False)
+        fn = ydl.prepare_filename(result)
+        print(fn)
+        #fn = ydl_opts["outtmpl"]
+    if type == "mp4":
+        name = os.path.splitext(fn)
+        directory = fn
+        if name[1] == ".mkv":
+            videooutput = 'ffmpeg -i "./{}" -vcodec copy -acodec -f mp4 "{}"'.format(fn,name[0]+".mp4")
+            os.system(videooutput)
+            directory = name[0]+".mp4"
+        if name[1] ==".webm":
+            videooutput = 'ffmpeg -i "./{}" -f mp4 "{}"'.format(fn,name[0]+".mp4")
+            os.system(videooutput)
+            directory = name[0]+".mp4"
+        if name[1] == ".mp4":
+            directory = name[0]+".mp4"
+        return directory
+    if type == "mp3":
+        name = os.path.splitext(fn)
+        return str(name[0])+".mp3"
+
+
+if __name__ == "__main__":
+    pass
---
+++ webserver.py
@@ -0,0 +1,61 @@
+import flask 
+import atexit
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request,send_from_directory,send_file
+from functools import wraps
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import func
+from webapps import youtube_api
+
+
+#                           Flask
+#################################################################
+website = Flask(__name__,
+            static_url_path='', 
+            static_folder='web/static',
+            template_folder='web/templates')
+website.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+#website.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/yagmurerva/Desktop/webserver.db'
+db = SQLAlchemy(website)
+downloadedfiles=[]
+alerts=[]
+
+@website.route('/')
+def index():
+    return render_template("mainpage.html")
+
+@website.route('/home')
+def home():
+    return render_template("mainpage.html")
+@website.route('/about')
+def about():
+    return render_template("about.html")
+
+@website.route('/youtube',methods = ["GET","POST"])
+def youtubeSearch():
+    if request.method == 'POST':
+        url = request.form['youtube-link']
+        avaibleformats = youtube_api.search_video(url)
+        url = url.split("=")[1]
+        alldata= [avaibleformats,url,alerts]
+        return render_template("select_page.html",alldata=alldata)
+    else:
+        return render_template("youtubepage.html")
+
+@website.route('/download/<string:url>/<string:type>-<string:id>',methods = ["GET","POST"])
+def youtubeDownload(url,type,id):
+    if request.method == 'POST':
+        return render_template("mainpage.html")
+    else:
+        file = youtube_api.download_source(url,type,id)
+        downloadedfiles.append(file)
+        return send_file(file,as_attachment=True)
+
+def cleaner():
+    import os
+    for i in downloadedfiles:
+        os.remove(i)
+atexit.register(cleaner)
+
+if __name__=="__main__":
+    website.run(debug=True)
